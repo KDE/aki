a23232063c77ad0067fcaa2f5a051ebf8e14f56f
Keith Rusler <xzekecomax@gmail.com>
2010-12-11 15:54:54 -0600

    Added documentation

--- libaki/interfaces/imaincontroller.hpp
+++ libaki/interfaces/imaincontroller.hpp
@@ -28,15 +28,55 @@ namespace Aki
 class DockWidget;
 class ISettingsPage;
 class Plugin;
+/**
+ * Interface class for calling the functions of the MainWindow class.
+ * Plugins use these functions to add docks, settings pages, etc to the
+ * running applications.
+ */
 class LIBAKI_EXPORT IMainController
 {
 public:
     virtual ~IMainController();
+    /**
+     * Addes a new @p dockWidget the application in the @p area specified.
+     *
+     * @param dockWidget DockWidget to be added.
+     * @param area Location of where the dockwidget should be added to.
+     */
     virtual void addDock(Aki::DockWidget* dockWidget, Qt::DockWidgetArea area) = 0;
+    /**
+     * Added the @p plugin to the application.
+     * \internal
+     *
+     * @param plugin Plugin to be added.
+     */
     virtual void addGui(Aki::Plugin* plugin) = 0;
+    /**
+     * Registers the @p page to the configuration dialogue.
+     *
+     * @param page Settings page.
+     */
     virtual void addSettingsPage(Aki::ISettingsPage* page) = 0;
+    /**
+     * Removes the @p dockWidget from the MainWindow.
+     * @note You are required to delete the @p dockWidget after removal.
+     *
+     * @param dockWidget DockWidget to remove.
+     */
     virtual void removeDock(Aki::DockWidget* dockWidget) = 0;
+    /**
+     * Removes the @p plugin from the application.
+     * \internal
+     *
+     * @param plugin Plugin to be removed.
+     */
     virtual void removeGui(Aki::Plugin* plugin) = 0;
+    /**
+     * Removes the @p page from the configuration dialogue.
+     * @note You are required to delete the @p page after removal.
+     *
+     * @param page Settings page to be removed.
+     */
     virtual void removeSettingsPage(Aki::ISettingsPage* page) = 0;
 }; // End of class IMainController.
 } // End of namespace Aki.
--- libaki/sql/database.hpp
+++ libaki/sql/database.hpp
@@ -48,15 +48,35 @@ class LIBAKI_EXPORT Database
     Q_PROPERTY(QString hostName READ hostName WRITE setHostName)
     Q_PROPERTY(QString userName READ userName WRITE setUserName)
 public:
+    /**
+     * Creates a new Database object.
+     *
+     * @param parent Parent of the object.
+     */
     explicit Database(QObject* parent = 0);
+    /**
+     * Creates a new Database object with the SQL @p type and option @p connectionName.
+     *
+     * @param type SQL type QSQLITE, QMYSQL, etc.
+     * @param connectionName Connection name for this connection. Defaults to defaultConnection.
+     * @param parent Parent of the object.
+     */
     explicit Database(const QString& type, const QString& connectionName = QLatin1String(QSqlDatabase::defaultConnection),
                       QObject* parent = 0);
     ~Database();
     /**
      * Closes the database.
      */
-    void close();;
+    void close();
+    /**
+     * Gets the connection name that was set in the constructor if any.
+     *
+     * @return QSqlDatabase::defaultConnection if the one in the constructor was not set.
+     */
     QString connectionName() const;
+    /**
+     *
+     */
     QString connectOptions() const;
     /**
      * Creates a database for the given T.
@@ -76,7 +96,17 @@ public:
      * @return Database connection.
      */
     QSqlDatabase database();
+    /**
+     * Gets the database name if using a SQL server to connect to or the path.
+     *
+     * @return SQL database name if using an SQL server else the path to the sql file.
+     */
     QString databaseName() const;
+    /**
+     * Gets the host name of the connection. Usually localhost.
+     *
+     * @return Host name of the server.
+     */
     QString hostName() const;
     /**
      * Gets the open state of the database.
@@ -84,16 +114,42 @@ public:
      * @return true if the database is open; false otherwise.
      */
     bool isOpen() const;
+    /**
+     * Opens the database connection for use.
+     *
+     * @return true if the database was successfully opened; false otherwise.
+     */
     bool open();
+    /**
+     * Opens the database connection for use with the specified @p user and @p password.
+     *
+     * @return true if the database was successfully opened; false otherwise.
+     */
     bool open(const QString& user, const QString& password);
+    /**
+     * Gets the password for the connection.
+     *
+     * @return Password for the connection if used; else empty QString.
+     */
     QString password() const;
+    /**
+     * Gets the port for the connection.
+     *
+     * @return Port for the connection.
+     */
     int port() const;
     /**
+     * Registers the @p t with the database.
+     * @internal internal use only.
      *
+     * @return true if the class was successfully registered; false otherwise.
      */
     static bool registerClass(Aki::Sql::Table* t);
     /**
+     * Registers the class with the database.
+     * @internal internal use only.
      *
+     * @return true if the class was successfully registered; false otherwise.
      */
     template<typename T> static bool registerClass();
     /**
@@ -108,13 +164,39 @@ public:
      * @return true if the table was drop; false otherwise.
      */
     template<typename T> bool remove();
+    /**
+     * Removes the database with the @p connectionName from the global database.
+     *
+     * @param connectionName Connection name of the database.
+     */
     static void removeDatabase(const QString& connectionName);
+    /**
+     *
+     */
     void setConnectOptions(const QString& options = QString());
+    /**
+     *
+     */
     void setDatabaseName(const QString& name);
+    /**
+     *
+     */
     void setHostName(const QString& host);
+    /**
+     *
+     */
     void setPassword(const QString& password);
+    /**
+     *
+     */
     void setPort(int port);
+    /**
+     *
+     */
     void setUserName(const QString& name);
+    /**
+     *
+     */
     QString userName() const;
 Q_SIGNALS:
     /**
@@ -182,6 +264,13 @@ public:
     }
 };
 
+/**
+ * Registers the class @p TYPE in the global table registry
+ * with the @p BAME.
+ *
+ * @param TYPE Table class to register.
+ * @param NAME Name to help with registering.
+ */
 #define AKI_REGISTER_SQL_TABLE(TYPE, NAME) \
     RegisterTable<TYPE> AKIreg##NAME;
 
