761e4bef6a8f508403a1a2c5c7b63c71c151f59a
Keith Rusler <xzekecomax@gmail.com>
2010-12-20 08:07:58 +0000

    Added documentation for ReplyInfo

--- libaki/irc/replyinfo.hpp
+++ libaki/irc/replyinfo.hpp
@@ -31,20 +31,83 @@ namespace Aki
 namespace Irc
 {
 class ReplyInfoPrivate;
+/**
+ * ReplyInfo is a class that wraps around a message that is received from the IRC server.
+ * It makes it easier to get specific information from the message.
+ *
+ * This class is implicitly shared. It's meant to be used on the stack.
+ */
 class LIBAKI_EXPORT ReplyInfo
 {
 public:
+    /**
+     * Empty ReplyInfo.
+     */
     ReplyInfo();
+    /**
+     * Constructs a ReplyInfo with the @p sender, @p command, @p message, @p params and a @p replyCode.
+     * The parameters @p params and @p replyCode are optional. If the message has a @p replyCode then
+     * the @p command will be empty and vice-versa.
+     *
+     * @param sender Sender of the message. This can be the server or a user.
+     * @param command The message command (empty if there is a replyCode set).
+     * @param message Raw message
+     * @param params Split form of @p message.
+     * @param replyCode IRC numeric reply (default Null numeric).
+     */
     explicit ReplyInfo(const Aki::Irc::NickInfo& sender, const QString& command, const QString& message,
                        const QStringList& params = QStringList(), Aki::Irc::ReplyCodes replyCode = Aki::Irc::RPL_NULL);
+    /**
+     * Copies @p other to this.
+     *
+     * @param other ReplyInfo to copy.
+     */
     ReplyInfo(const Aki::Irc::ReplyInfo& other);
+    /**
+     * Destroys the object.
+     */
     ~ReplyInfo();
+    /**
+     * Assigns @p other to this object.
+     *
+     * @param other ReplyInfo to assign.
+     */
     Aki::Irc::ReplyInfo& operator=(const Aki::Irc::ReplyInfo& other);
+    /**
+     * Gets the IRC command. This is empty if isNumeric is true.
+     *
+     * @return IRC command.
+     */
     QString command() const;
+    /**
+     * Gets the state of if the message is numeric.
+     *
+     * @return true if message is a numeric reply; other it is a command.
+     */
     bool isNumeric() const;
+    /**
+     * Gets the raw representation of the message.
+     *
+     * @return Raw IRC message.
+     */
     QString message() const;
+    /**
+     * Gets the numeric value of the reply.
+     *
+     * @return Numeric reply; Aki::Irc::RPL_NULL if this message is a command or invalid.
+     */
     Aki::Irc::ReplyCodes numeric() const;
+    /**
+     * Gets a array representation of the message broken down.
+     *
+     * @return Array representation of the message.
+     */
     QStringList params() const;
+    /**
+     * Gets the sender of the message. This can be the server or user who sent it.
+     *
+     * @return Sender of the message.
+     */
     Aki::Irc::NickInfo sender() const;
 private:
     QSharedDataPointer<Aki::Irc::ReplyInfoPrivate> _d;
