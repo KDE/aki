88ed2c6667020aeae307f2aded40e9acc76797cd
Keith Rusler <xzekecomax@gmail.com>
2010-12-26 05:44:06 +0000

    Added StartupReply for messages 0-5

---
+++ libaki/irc/startupreply.cxx
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2009-2010  Keith Rusler <xzekecomax@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+#include "startupreply.hpp"
+#include "private/startupreply_p.hpp"
+using namespace Aki;
+using namespace Irc;
+
+StartupReply::StartupReply()
+    : Aki::Irc::Reply(),
+    _d(new Aki::Irc::StartupReplyPrivate)
+{
+}
+
+StartupReply::StartupReply(const Aki::Irc::ReplyInfo& replyInfo)
+    : Aki::Irc::Reply(replyInfo),
+    _d(new Aki::Irc::StartupReplyPrivate)
+{
+    if (replyInfo.numeric() == Aki::Irc::RPL_MYINFO ||
+        replyInfo.numeric() == Aki::Irc::RPL_ISUPPORT) {
+        _d->message = replyInfo.params().join(" ");
+    } else {
+        _d->message = replyInfo.params().at(1);
+    }
+}
+
+StartupReply::StartupReply(const Aki::Irc::StartupReply& other)
+    : Aki::Irc::Reply(other),
+    _d(other._d)
+{
+}
+
+StartupReply::~StartupReply()
+{
+}
+
+Aki::Irc::StartupReply&
+StartupReply::operator=(const Aki::Irc::StartupReply& other)
+{
+    Aki::Irc::Reply::operator=(other);
+    _d = other._d;
+    return *this;
+}
+
+QString
+StartupReply::message() const
+{
+    return _d->message;
+}
