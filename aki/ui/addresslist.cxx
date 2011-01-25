bf9243c5e033c03ac166e3b7b82a2b27867d27d0
Keith Rusler <xzekecomax@gmail.com>
2011-01-24 19:32:43 -0600

    Added AddressList model as this one will be the default and the other one will soon be trashed.

---
+++ aki/ui/addresslist.cxx
@@ -0,0 +1,202 @@
+/*
+ * Copyright 2009-2011  Keith Rusler <xzekecomax@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+
+#include "addresslist.hpp"
+#include "sql/address.hpp"
+#include "sql/database.hpp"
+#include "sql/server.hpp"
+#include "ui/addressmodel.hpp"
+using namespace Aki;
+using namespace Sql;
+
+AddressList::AddressList(QWidget* parent)
+    : QListView(parent)
+{
+    _model = new Aki::AddressModel(this);
+    setModel(_model);
+}
+
+AddressList::~AddressList()
+{
+}
+
+void
+AddressList::addAddress(Aki::Sql::Address* address)
+{
+    // Performs check on address and inserts at the end of the list.
+    insertAddress(count(), address);
+}
+
+Aki::Sql::Address*
+AddressList::address(int row) const
+{
+    return addressFromIndex(_model->index(row));
+}
+
+Aki::Sql::Address*
+AddressList::addressFromIndex(const QModelIndex& index) const
+{
+    if (!index.isValid()) {
+        return 0;
+    }
+
+    return _model->addresses().at(index.row());
+}
+
+int
+AddressList::count() const
+{
+    return _model->rowCount();
+}
+
+Aki::Sql::Address*
+AddressList::currentAddress() const
+{
+    QModelIndex index = selectionModel()->currentIndex();
+    if (!index.isValid()) {
+        return 0;
+    }
+
+    return addressFromIndex(index);
+}
+
+int
+AddressList::currentRow() const
+{
+    QModelIndex index = selectionModel()->currentIndex();
+    if (!index.isValid()) {
+        return -1;
+    }
+
+    return index.row();
+}
+
+Aki::AddressList::List
+AddressList::findItems(const QString& name, Qt::MatchFlags flags) const
+{
+    Aki::AddressList::List list;
+
+    QModelIndexList indexes = _model->match(_model->index(0), Qt::DisplayRole, name, -1, flags);
+    foreach (const QModelIndex& index, indexes) {
+        list.append(addressFromIndex(index));
+    }
+
+    return list;
+}
+
+QModelIndex
+AddressList::indexFromAddress(Aki::Sql::Address* address)
+{
+    return _model->index(_model->addresses().indexOf(address));
+}
+
+void
+AddressList::insertAddress(int row, Aki::Sql::Address* address)
+{
+    Q_ASSERT(address);
+
+    if (_model->addresses().contains(address)) {
+        return;
+    }
+
+    _model->insertAddress(row, address);
+}
+
+void
+AddressList::removeAddress(int row)
+{
+    Aki::Sql::Address* address = takeAddress(row);
+    delete address;
+}
+
+void
+AddressList::repopulateAddresses(Aki::Sql::Server* server)
+{
+    Q_ASSERT(server);
+
+    for (int i = 0, c = count(); i < c; ++i) {
+        removeAddress(i);
+    }
+
+    const Aki::AddressList::List list = _database->find<Aki::Sql::Address>()
+        .where("addressServer = :addressServer").bind("addressServer", server->id())
+        .result();
+
+    foreach (Aki::Sql::Address* address, list) {
+        addAddress(address);
+    }
+}
+
+int
+AddressList::row(Aki::Sql::Address* address) const
+{
+    return _model->addresses().indexOf(address);
+}
+
+Aki::AddressList::List
+AddressList::selectedAddresses() const
+{
+    Aki::AddressList::List list;
+
+    const QModelIndexList indexes = selectionModel()->selectedRows();
+    foreach (const QModelIndex& index, indexes) {
+        list.append(addressFromIndex(index));
+    }
+
+    return list;
+}
+
+void
+AddressList::setCurrentAddress(Aki::Sql::Address* address, QItemSelectionModel::SelectionFlags command)
+{
+    selectionModel()->setCurrentIndex(indexFromAddress(address), command);
+}
+
+void
+AddressList::setCurrentAddress(Aki::Sql::Address* address)
+{
+    setCurrentAddress(address, QItemSelectionModel::ClearAndSelect);
+}
+
+void
+AddressList::setCurrentRow(int row, QItemSelectionModel::SelectionFlags command)
+{
+    selectionModel()->setCurrentIndex(_model->index(row), command);
+}
+
+void
+AddressList::setCurrentRow(int row)
+{
+    setCurrentRow(row, QItemSelectionModel::ClearAndSelect);
+}
+
+void
+AddressList::setDatabase(Aki::Sql::Database* database)
+{
+    Q_ASSERT(database);
+    _database = database;
+}
+
+Aki::Sql::Address*
+AddressList::takeAddress(int row)
+{
+    return _model->takeAddress(row);
+}
---
+++ aki/ui/addresslist.hpp
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2009-2011  Keith Rusler <xzekecomax@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+
+#ifndef AKI_ADDRESSLIST_HPP
+#define AKI_ADDRESSLIST_HPP
+
+#include <QtGui/QListView>
+
+namespace Aki
+{
+namespace Sql
+{
+class Address;
+class Database;
+class Server;
+} // End of namespace Sql.
+class AddressModel;
+class AddressList
+    : public QListView
+{
+    Q_OBJECT
+public:
+    typedef QList<Aki::Sql::Address*> List;
+    explicit AddressList(QWidget* parent = 0);
+    ~AddressList();
+    void addAddress(Aki::Sql::Address* address);
+    Aki::Sql::Address* address(int row) const;
+    int count() const;
+    Aki::Sql::Address* currentAddress() const;
+    int currentRow() const;
+    Aki::AddressList::List findItems(const QString& name, Qt::MatchFlags flags) const;
+    void insertAddress(int row, Aki::Sql::Address* address);
+    void removeAddress(int row);
+    int row(Aki::Sql::Address* address) const;
+    Aki::AddressList::List selectedAddresses() const;
+    void setCurrentAddress(Aki::Sql::Address* address, QItemSelectionModel::SelectionFlags command);
+    void setCurrentAddress(Aki::Sql::Address* address);
+    void setCurrentRow(int row, QItemSelectionModel::SelectionFlags command);
+    void setCurrentRow(int row);
+    void setDatabase(Aki::Sql::Database* database);
+    Aki::Sql::Address* takeAddress(int row);
+public Q_SLOTS:
+    void repopulateAddresses(Aki::Sql::Server* server);
+Q_SIGNALS:
+    void addressActivated(Aki::Sql::Address* address);
+    void addressChanged(Aki::Sql::Address* address);
+    void addressClicked(Aki::Sql::Address* address);
+    void addressCurrentRowChanged(int row);
+    void addressEntered(Aki::Sql::Address* address);
+    void addressPressed(Aki::Sql::Address* address);
+    void addressSelectionChanged();
+    void currentAddressChanged(Aki::Sql::Address* current, Aki::Sql::Address* previous);
+protected:
+    Aki::Sql::Address* addressFromIndex(const QModelIndex& index) const;
+    QModelIndex indexFromAddress(Aki::Sql::Address* address);
+private:
+    Aki::AddressModel* _model;
+    Aki::Sql::Database* _database;
+}; // End of class AddressList.
+} // End of namespace Aki.
+
+#endif // AKI_ADDRESSLIST_HPP
