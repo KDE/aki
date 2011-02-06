a61576c95b6ffd3857984ea7b0b2a69035aece22
Keith Rusler <xzekecomax@gmail.com>
2011-02-06 03:39:35 -0600

    Added ServerList class. Just need to write the widget and hook up the importing/exporting classes and should be done. I'll have to rewrite them and I will have mIRC servers importing, Aki's servers importing/exporting and XChat servers importing (I'll have to see how they do it and see what I can work with on that).

---
+++ aki/ui/serverlist.cxx
@@ -0,0 +1,229 @@
+/*
+ * Copyright 2009-2011  Keith Rusler <xzekecomax@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+#include "serverlist.hpp"
+#include "sql/database.hpp"
+#include "sql/server.hpp"
+#include "ui/servermodel.hpp"
+using namespace Aki;
+
+ServerList::ServerList(QWidget* parent)
+    : QListView(parent)
+{
+    _model = new Aki::ServerModel(this);
+    setModel(_model);
+
+    connect(this, SIGNAL(activated(QModelIndex)),
+            SLOT(slotItemActivated(QModelIndex)));
+    connect(_model, SIGNAL(dataChanged(QModelIndex,QModelIndex)),
+            SLOT(slotItemChanged(QModelIndex)));
+    connect(this, SIGNAL(clicked(QModelIndex)),
+            SLOT(slotItemClicked(QModelIndex)));
+    connect(this, SIGNAL(doubleClicked(QModelIndex)),
+            SLOT(slotItemDoubleClicked(QModelIndex)));
+    connect(this, SIGNAL(entered(QModelIndex)),
+            SLOT(slotItemEntered(QModelIndex)));
+    connect(this, SIGNAL(pressed(QModelIndex)),
+            SLOT(slotItemPressed(QModelIndex)));
+    connect(selectionModel(), SIGNAL(currentChanged(QModelIndex,QModelIndex)),
+            SLOT(slotItemCurrentChanged(QModelIndex,QModelIndex)));
+    connect(selectionModel(), SIGNAL(selectionChanged(QItemSelection,QItemSelection)),
+            SIGNAL(serverSelectionChanged()));
+}
+
+ServerList::~ServerList()
+{
+}
+
+void
+ServerList::addServer(Aki::Sql::Server* server)
+{
+    insertServer(count(), server);
+}
+
+int
+ServerList::count() const
+{
+    return model()->rowCount();
+}
+
+int
+ServerList::currentRow() const
+{
+    const QModelIndex index = selectionModel()->currentIndex();
+    if (!index.isValid()) {
+        return -1;
+    }
+
+    return index.row();
+}
+
+Aki::Sql::Server*
+ServerList::currentServer() const
+{
+    const QModelIndex index = selectionModel()->currentIndex();
+    if (!index.isValid()) {
+        return 0;
+    }
+
+    return serverFromIndex(index);
+}
+
+Aki::ServerList::List
+ServerList::findItems(const QString& name, Qt::MatchFlags flags) const
+{
+    Aki::ServerList::List list;
+
+    const QModelIndexList indexes = model()->match(_model->index(0), Qt::DisplayRole, name, -1, flags);
+    foreach (const QModelIndex& index, indexes) {
+        list.append(serverFromIndex(index));
+    }
+
+    return list;
+}
+
+QModelIndex
+ServerList::indexFromServer(Aki::Sql::Server* server)
+{
+    return _model->index(_model->servers().indexOf(server));
+}
+
+void
+ServerList::insertServer(int row, Aki::Sql::Server* server)
+{
+    Q_ASSERT(server);
+
+    if (_model->servers().contains(server)) {
+        return;
+    }
+
+    _model->insertServer(row, server);
+}
+
+void
+ServerList::removeServer(int row)
+{
+    delete takeServer(row);
+}
+
+int
+ServerList::row(Aki::Sql::Server* server) const
+{
+    return _model->servers().indexOf(server);
+}
+
+Aki::ServerList::List
+ServerList::selectedServers() const
+{
+    Aki::ServerList::List list;
+
+    const QModelIndexList indexes = selectionModel()->selectedRows();
+    foreach (const QModelIndex& index, indexes) {
+        list.append(serverFromIndex(index));
+    }
+
+    return list;
+}
+
+Aki::Sql::Server*
+ServerList::server(int row) const
+{
+    return serverFromIndex(_model->index(row));
+}
+
+Aki::Sql::Server*
+ServerList::serverFromIndex(const QModelIndex& index) const
+{
+    if (!index.isValid()) {
+        return 0;
+    }
+
+    return _model->servers().at(index.row());
+}
+
+void
+ServerList::setCurrentRow(int row, QItemSelectionModel::SelectionFlags command)
+{
+    selectionModel()->setCurrentIndex(_model->index(row), command);
+}
+
+void
+ServerList::setCurrentServer(Aki::Sql::Server* server, QItemSelectionModel::SelectionFlags command)
+{
+    selectionModel()->setCurrentIndex(indexFromServer(server), command);
+}
+
+void
+ServerList::setDatabase(Aki::Sql::Database* database)
+{
+    Q_ASSERT(database);
+    _database = database;
+}
+
+void
+ServerList::slotItemActivated(const QModelIndex& index)
+{
+    emit serverActivated(serverFromIndex(index));
+}
+
+void
+ServerList::slotItemChanged(const QModelIndex& index)
+{
+    emit serverChanged(serverFromIndex(index));
+}
+
+void
+ServerList::slotItemClicked(const QModelIndex& index)
+{
+    emit serverClicked(serverFromIndex(index));
+}
+
+void
+ServerList::slotItemCurrentChanged(const QModelIndex& current, const QModelIndex& previous)
+{
+    QPersistentModelIndex persistentCurrent = current;
+    Aki::Sql::Server* currentServer = server(persistentCurrent.row());
+    emit currentServerChanged(currentServer, server(previous.row()));
+    emit serverCurrentRowChanged(persistentCurrent.row());
+}
+
+void
+ServerList::slotItemDoubleClicked(const QModelIndex& index)
+{
+    emit serverDoubleClicked(serverFromIndex(index));
+}
+
+void
+ServerList::slotItemEntered(const QModelIndex& index)
+{
+    emit serverEntered(serverFromIndex(index));
+}
+
+void
+ServerList::slotItemPressed(const QModelIndex& index)
+{
+    emit serverPressed(serverFromIndex(index));
+}
+
+Aki::Sql::Server*
+ServerList::takeServer(int row)
+{
+    return _model->takeServer(row);
+}
---
+++ aki/ui/serverlist.hpp
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2009-2011  Keith Rusler <xzekecomax@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+#ifndef AKI_SERVERLIST_HPP
+#define AKI_SERVERLIST_HPP
+
+#include <QtGui/QListView>
+
+namespace Aki
+{
+namespace Sql
+{
+class Database;
+class Server;
+} // End of namespace Sql.
+class ServerModel;
+class ServerList
+    : public QListView
+{
+    Q_OBJECT
+public:
+    typedef QList<Aki::Sql::Server*> List;
+    explicit ServerList(QWidget* parent = 0);
+    ~ServerList();
+    void addServer(Aki::Sql::Server* server);
+    int count() const;
+    int currentRow() const;
+    Aki::Sql::Server* currentServer() const;
+    Aki::ServerList::List findItems(const QString& name, Qt::MatchFlags flags) const;
+    void insertServer(int row, Aki::Sql::Server* server);
+    void removeServer(int row);
+    int row(Aki::Sql::Server* server) const;
+    Aki::ServerList::List selectedServers() const;
+    Aki::Sql::Server* server(int row) const;
+    void setCurrentRow(int row, QItemSelectionModel::SelectionFlags command = QItemSelectionModel::ClearAndSelect);
+    void setCurrentServer(Aki::Sql::Server* server, QItemSelectionModel::SelectionFlags command = QItemSelectionModel::ClearAndSelect);
+    void setDatabase(Aki::Sql::Database* database);
+    Aki::Sql::Server* takeServer(int row);
+public Q_SLOTS:
+Q_SIGNALS:
+    void currentServerChanged(Aki::Sql::Server* current, Aki::Sql::Server* previous);
+    void serverActivated(Aki::Sql::Server* server);
+    void serverChanged(Aki::Sql::Server* server);
+    void serverClicked(Aki::Sql::Server* server);
+    void serverCurrentRowChanged(int row);
+    void serverDoubleClicked(Aki::Sql::Server* server);
+    void serverEntered(Aki::Sql::Server* server);
+    void serverPressed(Aki::Sql::Server* server);
+    void serverSelectionChanged();
+protected:
+    Aki::Sql::Server* serverFromIndex(const QModelIndex& index) const;
+    QModelIndex indexFromServer(Aki::Sql::Server* server);
+private Q_SLOTS:
+    void slotItemActivated(const QModelIndex& index);
+    void slotItemChanged(const QModelIndex& index);
+    void slotItemClicked(const QModelIndex& index);
+    void slotItemCurrentChanged(const QModelIndex& current, const QModelIndex& previous);
+    void slotItemDoubleClicked(const QModelIndex& index);
+    void slotItemEntered(const QModelIndex& index);
+    void slotItemPressed(const QModelIndex& index);
+private:
+    Aki::ServerModel* _model;
+    Aki::Sql::Database* _database;
+}; // End of class ServerList.
+} // End of namespace Aki.
+
+#endif // AKI_SERVERLIST_HPP
