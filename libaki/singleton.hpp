557cb6baefbd65f7a5b0d04466456fcb1140d3bd
Keith Rusler <xzekecomax@gmail.com>
2011-02-15 06:34:20 -0600

    Added documentation to Singleton

--- libaki/singleton.hpp
+++ libaki/singleton.hpp
@@ -6,11 +6,43 @@
 
 namespace Aki
 {
+/**
+ * Singleton is a class that will create a one time instance and is available
+ * in a global scope rather than a scope of a class. It is not copyable.
+ *
+ * To make a class a Singlton.
+ *
+ * foo.hpp
+ * @code
+ * class Foo : public Singleton<Foo>
+ * {
+ *     Q_OBJECT
+ * public:
+ *     ....
+ * private:
+ *     AKI_DECLARE_SINGLETON(Foo)
+ * };
+ * @endcode
+ *
+ * foo.cxx
+ * @code
+ * AKI_INIT_SINGLETON(Foo)
+ *
+ * ...
+ * @endcode
+ */
 template <typename T>
 class LIBAKI_EXPORT Singleton
     : public QObject
 {
 public:
+    /**
+     * Gets the instance of the Singleton.
+     * This will create an instance of the class if it has not already
+     * done so else it will return the instance.
+     *
+     * @return Instance of the class.
+     */
     static T* self()
     {
         if (!_instance) {
@@ -24,7 +56,7 @@ protected:
 } // End of namespace Aki.
 
 #define AKI_DECLARE_SINGLETON(klass) \
-    friend class Aki::Singleton<Aki::Sql::Database>; \
+    friend class Aki::Singleton<klass>; \
     klass(); \
     ~klass(); \
     Q_DISABLE_COPY(klass);
